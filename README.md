455eff942f361fc62cd3b64f5e20d1b3c2c18d65
Yoshiya Hinosawa <stibium121@gmail.com>
2015-04-23 23:37:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# event-hub v0.1.0
+
+> dom as an event hub
+
+***note*** `event-hub` depends on `jQuery` and [custom-class](https://github.com/kt3k/custom-class).
+
+# Usage
+
+```html
+<script src="path/to/jquery.js"></script>
+<script src="path/to/custom-class.js"></script>
+<script src="path/to/event-hub.js"></script>
+
+
+<div class="event-hub" channel="click">
+
+
+
+
+</div>
+```
+
+
+
---
+++ event-hub.js
@@ -0,0 +1,101 @@
+/**
+ * event-hub.js v0.1.0
+ * author: Yoshiya Hinosawa ( https://github.com/kt3k )
+ * license: MIT
+ */
+
+
+(function ($) {
+    'use strict';
+
+    $.registerCustomClass('event-hub', function () {
+
+        var ehub = new EventHub(this);
+
+        ehub.bindEvents();
+
+    });
+
+
+    /**
+     */
+    function EventHub(elem) {
+
+        this.$elem = $(elem);
+
+    };
+
+    var pt = EventHub.prototype;
+
+    /**
+     * Gets channels.
+     *
+     * @return {Array}
+     */
+    pt.channels = function () {
+
+        var channels = this.$elem.attr('channels') || this.$elem.attr('channel');
+
+        console.log(channels);
+
+        if (!channels) {
+            return [];
+        }
+
+        return channels.replace(/^\s*|\s*$/g, '').split(/\s+/);
+
+    };
+
+
+    /**
+     * Binds events.
+     */
+    pt.bindEvents = function () {
+
+        this.channels().forEach(this.bindEventsForChannel, this);
+
+    };
+
+
+    /**
+     * Binds to the given event (channel).
+     *
+     * The handler publish the event to the subscribers.
+     *
+     * @private
+     */
+    pt.bindEventsForChannel = function (channel) {
+
+        var $elem = this.$elem;
+
+        $elem.on(channel, function (e) {
+
+            // if the event is already processed by the hub
+            // then don't broadcast it again
+            if (e._eventHubProcessed) {
+                return;
+            }
+
+            // mark the event processed
+            e._eventHubProcessed = true;
+
+            var targets = $('.sub-' + channel, $elem);
+
+            targets.each(function () {
+
+                // if the original target is the same as subscriber
+                // then don't trigger it again
+                if (this !== e.target) {
+
+                    $(this).trigger(e);
+
+                }
+
+            });
+
+        });
+
+    };
+
+
+}(window.jQuery));

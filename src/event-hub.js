39831faa2f4594c83ced88eb59666919f88e761a
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-27 00:00:57 +0900

    refactor: switch to es2015, update class-component to v7

---
+++ src/event-hub.js
@@ -0,0 +1,65 @@
+/**
+ * event-hub.js v4.1.0
+ * author: Yoshiya Hinosawa ( https://github.com/kt3k )
+ * license: MIT
+ */
+
+/**
+ * EventHub is a coelement which forms event-hub class-component.
+ *
+ * see README for details.
+ */
+class EventHub {
+
+    constructor(elem) {
+        this.elem = elem
+
+        this.bindEvents()
+    }
+
+    /**
+     * Gets channels.
+     * @return {Array}
+     */
+    channels() {
+        const channels = this.elem.attr('channels') || this.elem.attr('channel')
+
+        if (!channels) {
+            return []
+        }
+
+        return channels.replace(/^\s*|\s*$/g, '').split(/\s+/)
+    }
+
+    /**
+     * Binds events.
+     */
+    bindEvents() {
+        this.channels().forEach(this.bindEventsForChannel, this)
+    }
+
+    /**
+     * Binds to the given event (channel).
+     *
+     * The handler publish the event to the subscribers.
+     * @private
+     */
+    bindEventsForChannel(channel) {
+        const elem = this.elem
+
+        elem.on(channel, function (e) {
+
+            const extraArgs = Array.prototype.slice.call(arguments, 1)
+
+            elem.find('.sub-' + channel).each(function () {
+                // if the original target is the same as subscriber
+                // then don't trigger it again
+                if (this !== e.target) {
+                    $(this).triggerHandler(e, extraArgs)
+                }
+            })
+        })
+    }
+}
+
+$.cc('event-hub', EventHub);
